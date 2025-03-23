e770d528b0a4fbca3703071f5b529eff78e3aebb
Kashyap <bobbikashyap@gmail.com>
2025-03-23 16:45:07 +0530

    - koin dependency injection initialization

--- composeApp/src/iosMain/kotlin/kashyap/learning/stocktrading/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/kashyap/learning/stocktrading/MainViewController.kt
@@ -2,4 +2,8 @@ package kashyap.learning.stocktrading
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initKoin()
+    }
+) { App() }
